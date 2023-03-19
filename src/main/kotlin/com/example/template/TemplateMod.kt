b6eabe12736d422accddea97c2548f5d00280833
Marvin Elsen <development@marvin-elsen.com>
2023-03-20 05:27:36 +0800

    Convert entry point to function

--- src/main/kotlin/com/example/template/TemplateMod.kt
+++ src/main/kotlin/com/example/template/TemplateMod.kt
@@ -1,12 +1,8 @@
 package com.example.template
 
-import net.fabricmc.api.ModInitializer
-
-// TODO: Update the values in fabric.mod.json
-// TODO: Rename all the filenames and directory and package names containing the modid placeholder
-// TODO: Implement your mod
-class TemplateMod : ModInitializer {
-
-    override fun onInitialize() {
-    }
+@Suppress("unused")
+fun init() {
+    TODO("Update the values in fabric.mod.json")
+    TODO("Rename all the filenames and directory and package names containing the modid placeholder")
+    TODO("Implement your mod")
 }
